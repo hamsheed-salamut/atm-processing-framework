30572a2f99779294af6b9879438a976d79bf7fe6
hsalamut@astek.mu <hsalamut@astek.mu>
2020-03-03 16:04:35 +0400

    Added Entity Frameworks & Services Extensions

---
+++ src/Transaction.Infrastructure/Context/TransactionContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transaction.Infrastructure.Context
+{
+    public class TransactionContext : DbContext
+    {
+    }
+}
---
+++ src/Transaction.Service/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Transaction.Infrastructure.Context;
+
+namespace Transaction.Service.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddTransactionFramework(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Service
+            //services.AddScoped<ITransactionService, TransactionService>();
+
+            // Repository
+            //services.AddScoped<IAccountSummaryRepository, AccountSummaryRepository>();
+            //services.AddScoped<IAccountTransactionRepository, AccountTransactionRepository>();
+
+            // Mappers
+            //services.AddAutoMapper(x => x.AddProfile(new MappingProfile()));
+
+            // Connection String
+            services.AddDbContext<TransactionContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection")));
+
+            return services;
+        }
+    }
+}
