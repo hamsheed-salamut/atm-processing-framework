9693371fcfc2a003f07c02b9d813ae90a6bf9d12
hsalamut@astek.mu <hsalamut@astek.mu>
2020-03-05 09:16:28 +0400

    Renamed Gateway project

--- src/Gateway.Api/Program.cs
+++ src/Gateway.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Gateway.Api
+namespace Gateway.WebApi
 {
     public class Program
     {
--- src/Gateway.Api/Startup.cs
+++ src/Gateway.Api/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Gateway.Api
+namespace Gateway.WebApi
 {
     public class Startup
     {
