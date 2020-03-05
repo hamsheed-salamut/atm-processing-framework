d2aed284db8dea0d7798c4df86de3376bb8d592c
hsalamut@astek.mu <hsalamut@astek.mu>
2020-03-05 14:20:54 +0400

    Implementation of User Service

---
+++ src/Identity.WebApi/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Identity.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.WebApi.Services
+{
+    public interface IUserService
+    {
+        bool Authenticate(string username, string password);
+    }
+
+    public class UserService : IUserService
+    {
+        private List<User> _users = new List<User>
+        {
+            new User { AccountNumber = 3628101, Currency = "EUR", FullName = "Hamsheed Salamut", Username = "hsalamut", Password = "hamsheed@gmail.com" }
+        };
+
+        public bool Authenticate(string username, string password)
+        {
+            var user = _users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            if (user != null)
+                return true;
+            return true;
+        }
+    }
+}
--- src/Identity.WebApi/Startup.cs
+++ src/Identity.WebApi/Startup.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Identity.WebApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Identity.WebApi
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddScoped<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
